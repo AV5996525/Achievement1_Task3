638aa5836d0d96ba75b3e402ebe4e779bbb14c0e
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-02 09:56:53 -0500

    Good

--- Task3.py
+++ Task3.py
@@ -6,6 +6,12 @@ while True:
             if i.isupper() :
                 fL = True
         return fL
+    def checkNameLc(str):
+        fLc = False
+        for i in str :
+            if i.islower() :
+                fLc = True
+        return fLc            
     def checkNum(str) :
         fN = False
         for i in str :
@@ -20,7 +26,9 @@ while True:
     elif nameUser.isdigit() :
         print("Username requires atleast one uppercase letter.")
     elif nameUser.isalpha() :
-        print("Username requires atleast one number.")        
+        print("Username requires atleast one number.")  
+    elif checkNameLc(nameUser) and checkNum(nameUser) :
+        print("Username has numbers, but is missing one uppercase letter.")          
     elif len(nameUser) > 1 and len(nameUser) <= 20 :
         if checkName(nameUser) == True and checkNum(nameUser) == True :
             print("Username meets requirements, congratulations!")
